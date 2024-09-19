dad63610806597a721e87876c482d144e30986a2
Jacobo Bouzas Quiroga <jacobo.bouzas@factorial.co>
2024-09-19 11:34:37 +0200

    chore: avoid unnecessary regex match

--- lib/prosopite.rb
+++ lib/prosopite.rb
@@ -160,7 +160,7 @@ module Prosopite
     def mysql_fingerprint(query)
       query = query.dup
 
-      return "mysqldump" if %r{\ASELECT /\*!40001 SQL_NO_CACHE \*/ \* FROM `}.match?(query)
+      return "mysqldump" if query.start_with?("SELECT /*!40001 SQL_NO_CACHE */ * FROM `")
 
       return "percona-toolkit" if %r{\*\w+\.\w+:[0-9]/[0-9]\*/}.match?(query)
       if (match = /\A\s*(call\s+\S+)\(/i.match(query))
@@ -178,38 +178,28 @@ module Prosopite
 
       query.gsub!(%r{/\*[^!].*?\*/}m, "")
       query.gsub!(/(?:--|#)[^\r\n]*(?=[\r\n]|\Z)/, "")
-
       return query if query.gsub!(/\Ause \S+\Z/i, "use ?")
 
       query.gsub!(/\\["']/, "")
       query.gsub!(/".*?"/m, "?")
       query.gsub!(/'.*?'/m, "?")
-
       query.gsub!(/\btrue\b|\bfalse\b/i, "?")
-
       query.gsub!(/[0-9+-][0-9a-f.x+-]*/, "?")
       query.gsub!(/[xb.+-]\?/, "?")
-
       query.strip!
       query.gsub!(/[ \n\t\r\f]+/, " ")
       query.downcase!
-
       query.gsub!(/\bnull\b/i, "?")
-
       query.gsub!(/\b(in|values?)(?:[\s,]*\([\s?,]*\))+/, '\\1(?+)')
-
       query.gsub!(
         /(?<!\w)field\s*\(\s*(\S+)\s*,\s*(\?+)(?:\s*,\s*\?+)*\)/,
         'field(\1, \2+)'
       )
-
       query.gsub!(
         /\b(select\s.*?)(?:(\sunion(?:\sall)?)\s\1)+/,
         '\\1 /*repeat\\2*/'
       )
-
       query.gsub!(/\blimit \?(?:, ?\?| offset \?)/, "limit ?")
-
       query.gsub!(/\G(.+?)\s+asc/, '\\1') if /\border by/.match?(query)
 
       query
